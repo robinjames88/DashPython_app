3bc0ce6088846b7c41059dd5447d62a623900c61
robinjames88 <64837002+robinjames88@users.noreply.github.com>
2020-06-05 03:44:02 +0800

    Add files via upload
    added html file

---
+++ liveupdate.py
@@ -0,0 +1,22 @@
+import dash
+import dash_html_components as html
+import dash_core_components as dcc
+from dash.dependencies import Input, Output
+import requests 
+
+# create dash application
+app = dash.DASH()
+
+
+
+# application layout
+app.layout = html.Div([
+            html.Div([])
+])
+
+
+
+def update_layout(n):
+    
+if __name__ == "__main__":
+    pass
